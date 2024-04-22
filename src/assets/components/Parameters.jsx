c50e575115ca015e80ca7bdb1d374c21ce7f3a6c
Witek <wiktorskid@wp.pl>
2024-04-22 19:41:39 +0200

    przed testem ze zdj

--- src/assets/components/Parameters.jsx
+++ src/assets/components/Parameters.jsx
@@ -2,45 +2,64 @@ import React from "react";
 import wood from "../images/parametry/wood2.png";
 import size from "../images/parametry/size.png";
 import shield from "../images/parametry/shield2.png";
+import polish from "../images/parametry/polisher.png";
 
 function Parameters() {
   return (
     <div className="max-w-[1080px]  mx-auto h-[100vh]">
-      <h1 className="text-[2.7rem] text-myOrange font-bold text-center">
-        Sam wybierz parametry <span className="text-myOrange">twojego</span>{" "}
-        produkty
+      <h1 className="text-[2.7rem]  font-bold text-center">
+        Albo powiedz mi jaki produkt cie interesuje <br></br>
       </h1>
-      <h1 className="text-[1.7rem]  text-center">
-        wybierz takie parametry jak{" "}
+      <h1 className="text-[2rem] text-gray-500  text-center">
+        a ja go dla ciebie wykonam
       </h1>
-      <div className="w-full mx-auto flex flex-row flex-wrap mt-16 space-x-10 items-center">
-        <div className="bg-[#e0e0e0] w-[48%]  rounded-xl flex flex-row items-center justify-center space-x-10 px-8 py-4">
+      <div className="w-full mx-auto flex flex-row  flex-wrap mt-12 justify-start gap-x-8 gap-y-8 items-center">
+        <div className="bg-[#e6e6e6] w-[48%] max-h-[210px] shadow-md shadow-[#00000024]  rounded-xl flex flex-row items-center justify-center space-x-10 px-8 py-4">
           <img src={wood} className="h-[150px]"></img>
-          <div className="flex flex-col pl-14">
-            <h1 className="text-3xl py-6">Rodzaj drewna</h1>
-            <h1 className="text-xl py-6 font-[Inter]">
+          <div className="flex flex-col w-full">
+            <h1 className="text-2xl font-semibold text-[#4d4d4d] py-6 font-[Helvetica]">
+              Rodzaj drewna
+            </h1>
+            <h1 className="text-lg font-semibold py-6 font-[Helvetica] text-gray-500 ">
               Jesion, dąb, sosna i wiele innych!
             </h1>
           </div>
         </div>
-        <div className="bg-[#e0e0e0] w-[48%]  rounded-xl flex flex-row items-center justify-center space-x-10 px-8 py-4">
+        <div className="bg-[#e6e6e6] w-[48%] max-h-[210px] text-[#4d4d4d] shadow-md shadow-[#00000024]  rounded-xl flex flex-row items-center justify-center space-x-10 px-8 py-4">
           <img src={size} className="h-[150px]"></img>
-          <div className="flex flex-col w-full pl-14 ">
-            <h1 className="text-3xl py-6 ">Wymiary produktu</h1>
-            <h1 className="text-xl py-6 font-[Inter]">
-              Jesion, dąb, sosna i wiele innych!
+          <div className="flex flex-col w-full ">
+            <h1 className="text-2xl py-6 font-[Helvetica] font-bold text-[#4d4d4d] ">
+              Wymiary produktu
+            </h1>
+            <h1 className="text-lg font-semibold py-6 font-[Helvetica]  text-gray-500">
+              Wymiary według twoich potrzeb
             </h1>
           </div>
         </div>
-        {/* <div className="bg-[#e0e0e0]  rounded-xl flex flex-row items-center justify-center  space-x-10 px-8 py-4 ">
-          <img src={size} className="h-[150px]"></img>
-          <div className="flex flex-col pl-14">
-            <h1 className="text-4xl py-6">Wymiary produktu</h1>
-            <h1 className="text-xl py-6 font-[Inter]">
-              Jesion, dąb, sosna i wiele innych!
+        {/* item 3 */}
+        <div className="bg-[#e6e6e6] max-h-[210px] w-[48%] shadow-md shadow-[#00000024]  rounded-xl flex flex-row items-center justify-center space-x-10 px-8 py-4">
+          <img src={shield} className="h-[150px]"></img>
+          <div className="flex flex-col w-full ">
+            <h1 className="text-2xl py-6 font-[Helvetica] font-semibold text-[#4d4d4d]">
+              Sposób impregnacji
+            </h1>
+            <h1 className="text-lg font-semibold py-6 font-[Helvetica] text-gray-500">
+              Olej? Lakier? Impregnat? A może wosk?
             </h1>
           </div>
-        </div> */}
+        </div>
+        {/* item 4 */}
+        <div className="bg-[#e6e6e6] max-h-[210px] w-[48%] shadow-md shadow-[#00000024]  rounded-xl flex flex-row items-center justify-center space-x-10 px-8 py-4">
+          <img src={polish} className="h-[150px]"></img>
+          <div className="flex flex-col w-full ">
+            <h1 className="text-2xl py-6 font-[Helvetica] font-semibold text-[#4d4d4d]">
+              Faktura powierzchni
+            </h1>
+            <h1 className="text-lg font-semibold py-6 font-[Helvetica] text-gray-500">
+              Zdecyduj jaką fakturę powinien mieć twój mebel
+            </h1>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/assets/components/Products.jsx
+++ src/assets/components/Products.jsx
@@ -8,19 +8,21 @@ function Products() {
     <div className="max-w-[1080px] pb-32  mx-auto px-10 ">
       {/* bg-[#e8e8e8] shadow-lg shadow-[#0000002a] rounded-b-xl */}
       <div className="flex flex-col justify-center items-center mx-auto pt-[90px]">
-        <h1 className="text-7xl font-bold ">Polecane Produkty</h1>
-        <h1 className="text-4xl border-b-2 border-myOrange py-2 px-12">
-          Nowoczesny design
+        <h1 className="text-5xl font-bold  ">Zobacz nasze gotowe produkty!</h1>
+        <h1 className="text-xl border-b-2 font-[roboto] tracking-tight text-gray-500 border-myOrange py-2 px-12">
+          uniwersalny wygląd pasujący do każdego stylu
         </h1>
         <div className="w-full grid grid-cols-1 gap-6 pt-20 max-w-full mx-auto  justify-center items-center">
           {/* ITEM1 */}
           <div className="w-[100%] h-[220px] bg-[#dcdcdc] hover:bg-[#d3d3d2] && duration-150 cursor-pointer rounded-[20px] mx-auto flex flex-row items-center justify-start ">
             <img
-              src={stol1}
+              src="https://ireland.apollo.olxcdn.com/v1/files/d0ppduzx0fy32-PL/image;s=1000x700"
               className="rounded-[20px] w-[264px]  h-[90%]  mx-4 "
             ></img>
-            <div className="mb-auto py-6 px-6 flex flex-col justify-between h-full ">
-              <h1 className="text-2xl">Stół Jesionowy do salonu</h1>
+            <div className="mb-auto py-6 px-6  flex flex-col justify-between h-full ">
+              <h1 className="text-2xl font-[Roboto]">
+                Stół Jesionowy do salonu
+              </h1>
               <div className="flex flex-col space-y-2">
                 <h1 className="text-xl text-orange-400">Jesion</h1>
                 <h1 className="text-xl text-[#7a7a7a]">160x90x80cm</h1>
@@ -36,11 +38,11 @@ function Products() {
           {/* ITEM2 */}
           <div className="w-[100%] h-[220px] bg-[#dcdcdc] hover:bg-[#d3d2d2] && duration-150 cursor-pointer rounded-[20px] mx-auto flex flex-row items-center justify-start">
             <img
-              src={stol2}
+              src="https://ireland.apollo.olxcdn.com/v1/files/ivux16d6ivk72-PL/image;s=1000x700"
               className="rounded-[20px] w-[264px]  h-[90%]  mx-4 "
             ></img>
             <div className="mb-auto py-6 px-6 flex flex-col justify-between h-full ">
-              <h1 className="text-2xl">Stolik kawowy</h1>
+              <h1 className="text-2xl font-[helvetica]">Stolik kawowy</h1>
               <div className="flex flex-col space-y-2">
                 <h1 className="text-xl  text-orange-400">Dąb</h1>
                 <h1 className="text-xl text-[#7a7a7a]">100x60x58cm</h1>
